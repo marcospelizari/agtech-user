60546fcb758968725bc0497ab46207779f6bcffe
Marcos Pelizari <m.pelizari@proton.me>
2026-05-10 19:02:19 -0300

    first commit

---
+++ src/main/java/com/agtech/user/UserApplication.java
@@ -0,0 +1,13 @@
+package com.agtech.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class, args);
+    }
+
+}
